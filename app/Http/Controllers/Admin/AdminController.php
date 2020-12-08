2ccc9495a92fe0308fa7d9f67b53b0bee81490c5
john joseph florina <devjosephflorina@gmail.com>
2020-12-08 17:30:57 +0800

    finish login in dashboard and logout

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -5,14 +5,30 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Session;
 use Hash;
+use Auth;
 
 class AdminController extends Controller
 {
     public function dashboard(){
         return view('admin.admin_dashboard');
     }
-    public function login(){
+    public function login(Request $request){
+        if($request->isMethod('post')){
+            $data = $request->all();
+            // echo '<pre>'; print_r($data); die;
+            if (Auth::guard('admin')->attempt(['email' => $data['email'], 'password' => $data['password']])){
+                return redirect('admin/dashboard');
+            }else{
+                Session::flash('error_message', 'Holy guacamole! You should check in on some of those fields below.');
+                return redirect()->back();
+            }
+        }
         return view('admin.admin_login');
+    }
+    public function logout(){
+        Auth::guard('admin')->logout();
+        return redirect('/admin');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,6 @@ Route::prefix('/admin')->namespace('Admin')->group(function(){
     Route::match(['get', 'post'], '/', 'AdminController@login');
     Route::group(['middleware' => ['admin']], function(){
         Route::get('dashboard', 'AdminController@dashboard');
+        Route::get('logout', 'AdminController@logout');
     });
 });
